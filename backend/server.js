287e9d9a6ddc556a6bec26f1779724179ec0b5d3
richa-sankineni <richasankineni1234@gmail.com>
2026-06-22 12:36:26 +0530

    Simplify CORS setup in server.js
    Removed CORS configuration and allowed origins.

--- backend/server.js
+++ backend/server.js
@@ -39,21 +39,7 @@ if (!process.env.GEMINI_API_KEY) {
 const app = express();
 
 app.use(helmet());
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://ai-travel-planner-ica1.vercel.app"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+app.use(cors())
 
 app.use(express.json());
 
